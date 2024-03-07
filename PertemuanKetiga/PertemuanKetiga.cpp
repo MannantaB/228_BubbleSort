bc8f16970f76e74da94ff6c9c645678a060fe7bd
MannantaB <mannanta18@gmail.com>
2024-03-07 15:55:07 +0700

    membuat proses output

--- PertemuanKetiga/PertemuanKetiga.cpp
+++ PertemuanKetiga/PertemuanKetiga.cpp
@@ -49,6 +49,19 @@ void bubbleSortArray() { //prosedur untuk mengurutkan array dengan metode
 
     } while (pass < n - 1); //step 5  
 }
+
+void display() {
+    cout << endl;
+    cout << "=============" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=============" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl; //output setiap element array pada garis baru
+
+    }
+    cout << "jumalahpass = " << n - 1 << endl; // menampilkan total angka dari elemen yang
+    cout << endl;
+}
 int main()
 {
     std::cout << "Hello World!\n";
